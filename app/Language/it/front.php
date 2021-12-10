416e8cf052eb1515a6d2fa3f8f24a3a0171671ff
pws <procreagency@gmail.com>
2021-12-10 19:45:45 +0100

    header & home

--- app/Language/it/front.php
+++ app/Language/it/front.php
@@ -1,5 +1,14 @@
 <?php
-$lang['member'] = 'Membri';
+$lang['btn_delete']="Elemina";
+$lang['btn_logout']="Logout";
+$lang['btn_login']= "Accedi";
+$lang['btn_signup']= "Registrati";
+$lang['btn_save']= "Salva";
 
+$lang['link_go']="partire!";
+$lang['title_my_cart']="Carello";
+$lang['title_checkout']="Checkout";
+$lang['title_total']="Totale";
+$lang['title_cours_by_categ']="corsi per categoria";
 return $lang;
 ?>
