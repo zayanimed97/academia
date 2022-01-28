7cfc1b37ef4072b505038ef7a04f7db57abb1f89
zayanimed97 <72516856+zayanimed97@users.noreply.github.com>
2022-01-28 18:12:05 +0100

    Delete aws_s3.php

--- app/Config/aws_s3.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Config;
-
-use CodeIgniter\Config\BaseConfig;
-
-class aws_s3 extends BaseConfig
-{
-    public $key = "AKIAS5IC2VCDZV4BTC5H";
-    public $secret = "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";
-}
